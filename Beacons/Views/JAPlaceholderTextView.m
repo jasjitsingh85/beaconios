ab6d8dfc433e0a47ad815f9bfc0a2ff2d34c30b6
Jeff Ames <jeffreymames@gmail.com>
2013-11-07 07:47:29 -0800

    remove observer for text change from placeholdertextview. hopefully fixes crash

--- Beacons/Views/JAPlaceholderTextView.m
+++ Beacons/Views/JAPlaceholderTextView.m
@@ -48,6 +48,11 @@
     return self;
 }
 
+- (void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
 - (void)setPlaceholderColor:(UIColor *)placeholderColor
 {
     _placeholderColor = placeholderColor;
