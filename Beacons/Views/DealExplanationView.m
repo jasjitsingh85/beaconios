8045c8f2294e42adbd0ae0f9f0d31ecf9a7d9c1b
Jeff Ames <jeffreymames@gmail.com>
2014-07-17 21:25:50 -0400

    deal explanation popup updates

--- Beacons/Views/DealExplanationView.m
+++ Beacons/Views/DealExplanationView.m
@@ -8,24 +8,64 @@
 
 #import "DealExplanationView.h"
 
+@interface DealExplanationView()
+
+@property (strong, nonatomic) UIImageView *imageView;
+@property (strong, nonatomic) UIView *backgroundView;
+
+@end
+
 @implementation DealExplanationView
 
 - (id)initWithFrame:(CGRect)frame
 {
     self = [super initWithFrame:frame];
-    if (self) {
-        // Initialization code
+    if (!self) {
+        return nil;
     }
+    self.frame = [UIScreen mainScreen].bounds;
+    self.backgroundView = [[UIView alloc] initWithFrame:self.bounds];
+    self.backgroundView.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.7];
+    [self addSubview:self.backgroundView];
+    
+    self.imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"dealExplanation"]];
+    self.imageView.center = CGPointMake(self.width/2.0, self.height/2.0);
+    self.imageView.userInteractionEnabled = YES;
+    [self addSubview:self.imageView];
+    
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(dismiss)];
+    [self addGestureRecognizer:tap];
+    
     return self;
 }
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect
+- (void)show
 {
-    // Drawing code
+    UIWindow *frontWindow = [[UIApplication sharedApplication] keyWindow];
+    [frontWindow.rootViewController.view addSubview:self];
+    self.backgroundView.alpha = 0;
+    [UIView animateWithDuration:0.3 animations:^{
+        self.backgroundView.alpha = 1;
+    }];
+    self.imageView.transform = CGAffineTransformMakeTranslation(0, -self.height + 100);
+    [UIView animateWithDuration:0.5 delay:0.2 usingSpringWithDamping:0.5 initialSpringVelocity:0.5 options:0 animations:^{
+        self.imageView.transform = CGAffineTransformIdentity;
+    } completion:nil];
 }
-*/
 
+- (void)dismiss
+{
+    [UIView animateWithDuration:0.5 animations:^{
+        self.backgroundView.alpha = 0;
+        CGFloat angle = -M_1_PI + (float) random()/RAND_MAX *2*M_1_PI;
+        CGAffineTransform transform = CGAffineTransformMakeTranslation(0, self.height);
+        transform = CGAffineTransformRotate(transform, angle);
+        self.imageView.transform = transform;
+    } completion:^(BOOL finished) {
+        if (self.dismissCompletionBlock) {
+            self.dismissCompletionBlock();
+        }
+        [self removeFromSuperview];
+    }];
+}
 @end
