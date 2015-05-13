a1cec155cca1c2e10867c3d4bb7bb9a5ed0237ca
Jasjit Singh <jazjit.singh@gmail.com>
2015-05-13 13:10:59 -0700

    added gold coin

--- Beacons/RewardsViewController.m
+++ Beacons/RewardsViewController.m
@@ -21,6 +21,7 @@
 @property (nonatomic, strong) RewardExplanationPopupView *rewardExplanationPopupView;
 @property (nonatomic, strong) RewardsStoreViewController *rewardsStoreViewController;
 @property (nonatomic, strong) UIBarButtonItem *cancelButtonItem;
+@property (nonatomic, strong) UINavigationController *navigationController;
 
 @end
 
@@ -32,6 +33,7 @@
     self.navItem = navItem;
     self.rewardExplanationPopupView = [[RewardExplanationPopupView alloc] init];
     self.rewardsStoreViewController = [[RewardsStoreViewController alloc] init];
+    self.navigationController = [[UINavigationController alloc] initWithRootViewController:self.rewardsStoreViewController];
     self.cancelButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Done" style:UIBarButtonItemStyleDone target:self action:@selector(modalDoneButtonTouched:)];
     if (!self) {
         return nil;
@@ -44,8 +46,16 @@
 {
     [[APIClient sharedClient] getRewardsScore:^(AFHTTPRequestOperation *operation, id responseObject) {
         NSString *rewards_score = responseObject[@"total_rewards"];
-        self.rewards_score_nav_item = [UIButton navButtonWithTitle:[NSString stringWithFormat:@"%@", rewards_score]];
+//        self.rewards_score_nav_item = [UIButton navButtonWithTitle:[NSString stringWithFormat:@"%@", rewards_score]];
+        self.rewards_score_nav_item = [UIButton buttonWithType:UIButtonTypeCustom];
+        [self.rewards_score_nav_item setFrame:CGRectMake(0, 0, 20, 20)];
+        [self.rewards_score_nav_item setBackgroundImage:[UIImage imageNamed:@"goldCoin"] forState:UIControlStateNormal];
+        [self.rewards_score_nav_item setImageEdgeInsets:UIEdgeInsetsMake(0.0, 100, 28.0, 0.0)];
+        [self.rewards_score_nav_item setTitleEdgeInsets:UIEdgeInsetsMake(0, -33, 0.0, 10.0)];
+        [self.rewards_score_nav_item setTitle:[NSString stringWithFormat:@"%@", rewards_score] forState:UIControlStateNormal];
+        [self.rewards_score_nav_item setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
         [self.rewards_score_nav_item addTarget:self action:@selector(rewardsButtonTouched:) forControlEvents:UIControlEventTouchUpInside];
+        [self.rewards_score_nav_item.titleLabel setFont:[ThemeManager mediumFontOfSize:16]];
         self.navItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:self.rewards_score_nav_item];
     } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
         //self.rewards_score_nav_item = [UIButton navButtonWithTitle:@"ERROR"];
@@ -56,19 +66,18 @@
 {
     [self updateRewardsScore];
     //[self.rewardExplanationPopupView show];
-    UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:self.rewardsStoreViewController];
     
     //navigationController.navigationBar.topItem.title = @"ONE TIME SETUP";
     //navigationController.navigationBar.barTintColor = [[ThemeManager sharedTheme] redColor];
     //navigationController.navigationBar.titleTextAttributes = @{NSForegroundColorAttributeName : [UIColor whiteColor], NSFontAttributeName : [ThemeManager lightFontOfSize:18]};
     //navigationController.navigationBar.tintColor = [UIColor whiteColor];
-    [navigationController.navigationBar setBackgroundImage:    [[ThemeManager sharedTheme] navigationBackgroundForBarMetrics:UIBarMetricsDefault] forBarMetrics:UIBarMetricsDefault];
+    [self.navigationController.navigationBar setBackgroundImage:    [[ThemeManager sharedTheme] navigationBackgroundForBarMetrics:UIBarMetricsDefault] forBarMetrics:UIBarMetricsDefault];
     //self.navItem.leftBarButtonItem = self.cancelButtonItem;
 //    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemCancel target:self action:@selector(dismissModalViewControllerAnimated:)];
     
-    navigationController.modalTransitionStyle = UIModalTransitionStyleCoverVertical;
+    //self.navigationController.modalTransitionStyle = UIModalTransitionStyleCoverVertical;
     
-    [self presentViewController:navigationController
+    [self presentViewController:self.navigationController
                        animated:YES
                      completion:nil];
 }
