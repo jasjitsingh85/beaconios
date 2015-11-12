8187d36e9b84c54fca94d44e1e19fff30b30693c
Jasjit Singh <Jas@Jasjits-MacBook-Pro-854.local>
2015-11-11 20:12:14 -0800

    updates

---
+++ Beacons/WLHorizontalSegmentedControl.h
@@ -0,0 +1,13 @@
+//
+//  WLHorizontalSegmentedControl.h
+//  WLSegmentedControls
+//
+//  Created by Wang Ling on 7/27/10.
+//  Copyright 2010 I Wonder Phone. All rights reserved.
+//
+
+#import "WLSegmentedControl.h"
+
+@interface WLHorizontalSegmentedControl : WLSegmentedControl 
+
+@end
--- FilterViewController.m
+++ FilterViewController.m
@@ -21,6 +21,15 @@
 @property (readonly) NSInteger hotspotToggle;
 @property (readonly) NSInteger happyHourToggle;
 
+@property (assign, nonatomic) BOOL isHotspotToggleOn;
+@property (assign, nonatomic) BOOL isHappyHourToggleOn;
+
+@property (assign, nonatomic) BOOL isHotspotNow;
+@property (assign, nonatomic) BOOL isHotspotUpcoming;
+@property (assign, nonatomic) BOOL isHappyHourNow;
+@property (assign, nonatomic) BOOL isHappyHourUpcoming;
+
+
 @end
 
 #define selectedTransform CGAffineTransformMakeScale(1.35, 1.35)
@@ -54,6 +63,18 @@
     
     [self.view addSubview:self.tableView];
     
+    [self initializeFilter];
+    
+}
+
+-(void)initializeFilter
+{
+    self.isHotspotToggleOn = YES;
+    self.isHappyHourToggleOn = YES;
+    self.isHotspotNow = YES;
+    self.isHotspotUpcoming = YES;
+    self.isHappyHourNow = YES;
+    self.isHappyHourUpcoming = YES;
 }
 
 - (NSInteger)labelHeader
@@ -78,24 +99,28 @@
 
 - (void)collapseSection:(NSInteger)section
 {
-    [self.collapsedSections addObject:@(section)];
-    NSInteger numRowsExpanded = [self tableView:self.tableView numberOfRowsInExpandedSection:section];
-    NSMutableArray *indexPathsToDelete = [[NSMutableArray alloc] init];
-    for (NSInteger i=0; i<numRowsExpanded; i++) {
-        [indexPathsToDelete addObject:[NSIndexPath indexPathForRow:i inSection:section]];
+    if (![self sectionIsCollapsed:section]) {
+        [self.collapsedSections addObject:@(section)];
+        NSInteger numRowsExpanded = [self tableView:self.tableView numberOfRowsInExpandedSection:section];
+        NSMutableArray *indexPathsToDelete = [[NSMutableArray alloc] init];
+        for (NSInteger i=0; i<numRowsExpanded; i++) {
+            [indexPathsToDelete addObject:[NSIndexPath indexPathForRow:i inSection:section]];
+        }
+        [self.tableView deleteRowsAtIndexPaths:indexPathsToDelete withRowAnimation:UITableViewRowAnimationFade];
     }
-    [self.tableView deleteRowsAtIndexPaths:indexPathsToDelete withRowAnimation:UITableViewRowAnimationFade];
 }
 
 - (void)uncollapseSection:(NSInteger)section
 {
-    [self.collapsedSections removeObject:@(section)];
-    NSInteger numRowsExpanded = [self tableView:self.tableView numberOfRowsInExpandedSection:section];
-    NSMutableArray *indexPathsToInsert = [[NSMutableArray alloc] init];
-    for (NSInteger i=0; i<numRowsExpanded; i++) {
-        [indexPathsToInsert addObject:[NSIndexPath indexPathForRow:i inSection:section]];
+    if ([self sectionIsCollapsed:section]) {
+        [self.collapsedSections removeObject:@(section)];
+        NSInteger numRowsExpanded = [self tableView:self.tableView numberOfRowsInExpandedSection:section];
+        NSMutableArray *indexPathsToInsert = [[NSMutableArray alloc] init];
+        for (NSInteger i=0; i<numRowsExpanded; i++) {
+            [indexPathsToInsert addObject:[NSIndexPath indexPathForRow:i inSection:section]];
+        }
+        [self.tableView insertRowsAtIndexPaths:indexPathsToInsert withRowAnimation:UITableViewRowAnimationFade];
     }
-    [self.tableView insertRowsAtIndexPaths:indexPathsToInsert withRowAnimation:UITableViewRowAnimationFade];
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
@@ -127,7 +152,13 @@
         title.textColor = [UIColor blackColor];
         [view addSubview:title];
         title.text = [self tableView:tableView titleForHeaderInSection:section];
-        //[self setSelectAllButton:button forSection:section];
+        UISwitch *switchView = [[UISwitch alloc] initWithFrame:CGRectMake(self.view.width - 70, 5, 100, 30)];
+        switchView.on = YES;
+        switchView.transform = CGAffineTransformMakeScale(0.85, .8);
+        switchView.tag = section;
+        switchView.onTintColor = [[ThemeManager sharedTheme] redColor];
+        [switchView addTarget:self action:@selector(updateSwitchAtIndexPath:) forControlEvents:UIControlEventTouchUpInside];
+        [view addSubview:switchView];
         view.tag = section;
         UITapGestureRecognizer *headerTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(headerTapped:)];
         [view addGestureRecognizer:headerTap];
@@ -140,6 +171,11 @@
 - (void)headerTapped:(UITapGestureRecognizer *)tap
 {
     NSInteger section = tap.view.tag;
+    [self collapseOrUncollapseSection:section];
+}
+
+-(void)collapseOrUncollapseSection:(NSInteger)section
+{
     if ([self sectionIsCollapsed:section]) {
         [self uncollapseSection:section];
     }
@@ -196,37 +232,57 @@
     if (!cell) {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
         cell.selectionStyle = UITableViewCellSelectionStyleNone;
+        UIButton *nowButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        nowButton.size = CGSizeMake(150, 30);
+        nowButton.x = self.view.width/2 - 149.5;
+        nowButton.y = 5;
+        nowButton.layer.borderColor = [[ThemeManager sharedTheme] redColor].CGColor;
+        nowButton.layer.borderWidth = 1;
+        UIBezierPath *maskPath;
+        maskPath = [UIBezierPath bezierPathWithRoundedRect:nowButton.bounds
+                                         byRoundingCorners:(UIRectCornerBottomLeft|UIRectCornerTopLeft)
+                                               cornerRadii:CGSizeMake(2.0, 2.0)];
+        CAShapeLayer *maskLayer = [[CAShapeLayer alloc] init];
+        maskLayer.frame = nowButton.bounds;
+        maskLayer.path = maskPath.CGPath;
+        nowButton.layer.mask = maskLayer;
+        nowButton.backgroundColor = [[ThemeManager sharedTheme] redColor];
+        [nowButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+        [nowButton setTitleColor:[[UIColor whiteColor] colorWithAlphaComponent:0.5] forState:UIControlStateHighlighted];
+        [nowButton setTitle:@"NOW" forState:UIControlStateNormal];
+        nowButton.titleLabel.font = [ThemeManager mediumFontOfSize:10];
+        nowButton.tag = indexPath.section;
+        [nowButton addTarget:self action:@selector(nowButtonTapped:) forControlEvents:UIControlEventTouchUpInside];
         
-        UILabel *nameLabel = [[UILabel alloc] init];
-        CGRect frame = CGRectZero;
-        frame.size = CGSizeMake(160, tableView.rowHeight);
-        frame.origin.x = 15;
-        frame.origin.y = 0.5*(cell.contentView.frame.size.height - frame.size.height);
-        nameLabel.frame = frame;
-        nameLabel.autoresizingMask = UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleTopMargin;
-        nameLabel.backgroundColor = [UIColor clearColor];
-        nameLabel.textColor = [UIColor blackColor];
-        nameLabel.font = [ThemeManager lightFontOfSize:14];
-        nameLabel.adjustsFontSizeToFitWidth = YES;
-        nameLabel.tag = TAG_NAME_LABEL;
-        [cell.contentView addSubview:nameLabel];
+        UIButton *upcomingButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        upcomingButton.size = CGSizeMake(150, 30);
+        upcomingButton.x = self.view.width/2 - .5;
+        upcomingButton.y = 5;
+        upcomingButton.layer.borderColor = [[ThemeManager sharedTheme] redColor].CGColor;
+        upcomingButton.layer.borderWidth = 1;
+        UIBezierPath *maskPathUpcoming;
+        maskPathUpcoming = [UIBezierPath bezierPathWithRoundedRect:upcomingButton.bounds
+                                         byRoundingCorners:(UIRectCornerBottomRight|UIRectCornerTopRight)
+                                               cornerRadii:CGSizeMake(2.0, 2.0)];
+        CAShapeLayer *maskLayerUpcoming = [[CAShapeLayer alloc] init];
+        maskLayerUpcoming.frame = upcomingButton.bounds;
+        maskLayerUpcoming.path = maskPathUpcoming.CGPath;
+        upcomingButton.layer.mask = maskLayerUpcoming;
+        upcomingButton.backgroundColor = [[ThemeManager sharedTheme] redColor];
+        [upcomingButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+        [upcomingButton setTitleColor:[[UIColor whiteColor] colorWithAlphaComponent:0.5] forState:UIControlStateHighlighted];
+        [upcomingButton setTitle:@"UPCOMING" forState:UIControlStateNormal];
+        upcomingButton.titleLabel.font = [ThemeManager mediumFontOfSize:10];
+        upcomingButton.tag = indexPath.section;
+        [upcomingButton addTarget:self action:@selector(upcomingButtonTapped:) forControlEvents:UIControlEventTouchUpInside];
         
-        UIImageView *addFriendImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"addFriendNormal"]];
-        frame = addFriendImageView.frame;
-        frame.size = CGSizeMake(30, 30);
-        frame.origin.x = self.view.width - 60;
-        frame.origin.y = 0.5*(cell.contentView.frame.size.height - frame.size.height);
-        addFriendImageView.frame = frame;
-        addFriendImageView.contentMode = UIViewContentModeCenter;
-        addFriendImageView.autoresizingMask = UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleTopMargin;
-        addFriendImageView.tag = TAG_CHECK_IMAGE;
-        [cell.contentView addSubview:addFriendImageView];
+        UIView *middleBorder = [[UIView alloc] initWithFrame:CGRectMake(self.view.width/2 - .25, 6, 1, upcomingButton.frame.size.height - 2)];
+        middleBorder.backgroundColor = [UIColor whiteColor];
+        
+        [cell.contentView addSubview:nowButton];
+        [cell.contentView addSubview:upcomingButton];
+        [cell.contentView addSubview:middleBorder];
     }
-    
-    UILabel *nameLabel = (UILabel *)[cell.contentView viewWithTag:TAG_NAME_LABEL];
-    UIImageView *addFriendImageView = (UIImageView *)[cell.contentView viewWithTag:TAG_CHECK_IMAGE];
-    NSString *normalizedPhoneNumber;
-
     return cell;
 }
 
@@ -235,6 +291,78 @@
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
 
+}
+
+-(void)nowButtonTapped:(UIButton *)nowButton {
+    
+    if (nowButton.tag == self.hotspotToggle) {
+        self.isHotspotNow = !self.isHotspotNow;
+        [self updateButton:nowButton onState:self.isHotspotNow];
+    } else if (nowButton.tag == self.happyHourToggle) {
+        self.isHappyHourNow = !self.isHappyHourNow;
+        [self updateButton:nowButton onState:self.isHappyHourNow];
+    }
+    NSLog(@"Hotspot: %d", self.isHotspotNow);
+    NSLog(@"Happy Hour: %d", self.isHappyHourNow);
+}
+
+-(void)upcomingButtonTapped:(UIButton *)upcomingButton {
+    
+    if (upcomingButton.tag == self.hotspotToggle) {
+        self.isHotspotUpcoming = !self.isHotspotUpcoming;
+        [self updateButton:upcomingButton onState:self.isHotspotUpcoming];
+    } else if (upcomingButton.tag == self.happyHourToggle) {
+        self.isHappyHourUpcoming = !self.isHappyHourUpcoming;
+        [self updateButton:upcomingButton onState:self.isHappyHourUpcoming];
+    }
+    NSLog(@"Hotspot: %d", self.isHotspotUpcoming);
+    NSLog(@"Happy Hour: %d", self.isHappyHourUpcoming);
+}
+
+-(void)updateButton:(UIButton *)button onState:(BOOL)active
+{
+    if (active) {
+        button.backgroundColor = [[ThemeManager sharedTheme] redColor];
+        [button setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    } else {
+        button.backgroundColor = [UIColor whiteColor];
+        [button setTitleColor:[[ThemeManager sharedTheme] redColor] forState:UIControlStateNormal];
+    }
+}
+
+- (void)updateSwitchAtIndexPath:(UISwitch *)_switch{
+    if ([_switch isOn]) {
+        if (_switch.tag == self.hotspotToggle) {
+            self.isHotspotToggleOn = YES;
+        } else if (_switch.tag == self.happyHourToggle) {
+            self.isHappyHourToggleOn = YES;
+        }
+    } else {
+        if (_switch.tag == self.hotspotToggle) {
+            self.isHotspotToggleOn = NO;
+        } else if (_switch.tag == self.happyHourToggle) {
+            self.isHappyHourToggleOn = NO;
+        }
+    }
+    
+    [self reloadRowsAndSections];
+    
+}
+
+-(void)reloadRowsAndSections
+{
+    if (self.isHotspotToggleOn) {
+        [self uncollapseSection:self.hotspotToggle];
+    } else {
+        [self collapseSection:self.hotspotToggle];
+    }
+    
+    if (self.isHappyHourToggleOn) {
+        [self uncollapseSection:self.happyHourToggle];
+    } else {
+        [self collapseSection:self.happyHourToggle];
+    }
+    
 }
 
 -(void)dismissView:(id)sender
